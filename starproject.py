668571c95b3e97cc90f97b01687bfb2746bfbf59
Manas Mudbari <manasxaviers@gmail.com>
2016-08-18 09:02:05 -0700

    additional user profile parameters

--- starproject.py
+++ starproject.py
@@ -13,10 +13,10 @@ from urllib2 import Request, urlopen
 import json
 
 ##----Reading JSON from Github API (currently commented our)
-#starGaze = pd.read_json('https://api.github.com/repos/realm/jazzy/stargazers')
+starGaze = pd.read_json('https://api.github.com/repos/realm/jazzy/stargazers')
 
 ##----Reshaping the DataFrame into a matrix
-#user_str = starGaze['login'].as_matrix()
+user_str = starGaze['login'].as_matrix()
 
 ##----Searilizing and storing the API data locally
 user_out = open('user.pickle','wb')
@@ -27,20 +27,20 @@ user_out.close()
 read_user = pd.read_pickle('user.pickle')
 
 ##----Initializing column name for new user DataFrame
-columns = ['Name', 'Github Handle', 'Email', 'Company', 'Location']
+columns = ['Name', 'Github Handle', 'Email', 'Company', 'Location', 'Blog', 'Followers', 'Following']
 
 ##----Initializing empty placeholder list
 profile_list = []
 
 ##----Grabbing stargazer user profile
-for i in range(len(read_user)):
+for i in range(2):
     user_html = 'https://api.github.com/users/'+read_user[i]
-    request = Request(user_html, headers=hdr)
+    request = Request(user_html)
     response = urlopen(request)
     user_read = response.read()
     user = json.loads(user_read)
     ##----Creating a list of specific values from Github user profile
-    temp_list = str(user['name']), str(user['login']), str(user['email']), str(user['company']), str(user['location'])
+    temp_list = str(user['name']), str(user['login']), str(user['email']), str(user['company']), str(user['location']), str(user['blog']), int(user['followers']), int(user['following'])
     ##----concatenating the list to placeholder list
     profile_list.append(temp_list)
 
