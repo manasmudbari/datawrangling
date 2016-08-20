979e02fb2e9d21935fd7a4e2f3ca56aab676a124
Manas Mudbari <manasxaviers@gmail.com>
2016-08-20 16:32:44 -0700

    Final submission

--- realm_analysis.py
+++ realm_analysis.py
@@ -18,48 +18,49 @@ sys.setdefaultencoding('utf-8')
 
 
 #This code reads our exported CSV file from previous script
-df = pd.read_csv('realm.csv')
+df = pd.read_csv('realm_new.csv') #-- type the name of your exported CSV here        
 
-##Grouping by user's location
-new_df = df.groupby('Location')
-location_count = new_df['Location'].count()
+def location_plot():
+    ##Grouping by user's location
+    new_df = df.groupby('Location')
+    location_count = new_df['Location'].count()
+    ## This code converts user's location info into list for analysis (bar chart)
+    x1 = pd.Series(df['Location']).unique()
+    x1.sort()
+    x1_pos = np.arange(len(x1))
+    y1 = location_count.as_matrix()
+    
+    plt.bar(x1_pos, y1, align='center', color='#59569e', alpha=0.7, width=0.35)
+    plt.xticks(x1_pos, x1, rotation=85)
+    plt.xlabel('Stargazer\'s Location')
+    plt.ylabel('Stargazer Count')
+    plt.title('Realm repo stargazers count by location')
+    plt.tight_layout()
+    plt.show()
 
-
-## This code converts user's location info into list for analysis (bar chart)
-x1 = pd.Series(df['Location']).unique()
-x1.sort()
-x1_pos = np.arange(len(x1))
-y1 = location_count.as_matrix()
-
-##Grouping by user's company
-company_df = df.groupby('Company')
-company_count = new_df['Company'].count()
-
-## This code converts user's company info into list for analysis (bar chart)
-x2 = pd.Series(df['Company']).unique()
-x2.sort()
-x2_pos = np.arange(len(x2))
-y2 = company_count.as_matrix()
-
-#This following code is used to plot the bar chart & maps into one single figure
-plt.subplot(2,1,1)
-plt.bar(x1_pos, y1, align='center', color='#59569e', alpha=0.7, width=0.35)
-plt.xticks(x1_pos, x1)
-plt.xlabel('Stargazer\'s Location')
-plt.ylabel('Stargazer Count')
-plt.title('Realm repo stargazers count by location')
-
-plt.subplot(2,1,2)
-plt.bar(x2_pos, y2, align='center', color='#d34ca3', alpha=0.7, width=0.35)
-plt.xticks(x2_pos, x2)
-plt.xlabel('Stargazer\'s Company')
-plt.ylabel('Stargazer Count')
-plt.title('Realm repo stargazers count by company')
-
-plt.tight_layout()
-plt.show()
+def company_plot():
+    ##Grouping by user's company
+    company_df = df.groupby('Company')
+    company_count = company_df['Company'].count()
+    
+    ## This code converts user's company info into list for analysis (bar chart)
+    x2 = pd.Series(df['Company']).unique()
+    x2.sort()
+    x2_pos = np.arange(len(x2))
+    y2 = company_count.as_matrix()
+    
+    plt.bar(x2_pos, y2, align='center', color='#d34ca3', alpha=0.7, width=0.35)
+    plt.xticks(x2_pos, x2, rotation=85)
+    plt.xlabel('Stargazer\'s Company')
+    plt.ylabel('Stargazer Count')
+    plt.title('Realm repo stargazers count by company')
+    plt.tight_layout()
+    plt.show()
 
 
+##Remove # from code below to plot stargazers number by company name
+#company_plot()
 
-
+##Remove # from code below to plot stargazers number by location
+location_plot()
     
